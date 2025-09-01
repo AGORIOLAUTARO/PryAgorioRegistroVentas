30fc3109678179adf11aae43170f53c7a6a963a6
AGORIOLAUTARO <lautaroagorio@gmail.com>
2025-09-01 13:41:36 -0300

    correciones

--- PryAgorioRegistroVentas/frmRegistro.Designer.cs
+++ PryAgorioRegistroVentas/frmRegistro.Designer.cs
@@ -34,83 +34,116 @@
             lblPrecioUnitario = new Label();
             dtpFecha = new DateTimePicker();
             cmbProducto = new ComboBox();
-            cmbCantidad = new ComboBox();
             txtPrecioUnitario = new TextBox();
+            btnRegistrar = new Button();
+            txtCantidad = new TextBox();
+            btnCancelar = new Button();
             SuspendLayout();
             // 
             // lblFecha
             // 
             lblFecha.AutoSize = true;
-            lblFecha.Location = new Point(106, 92);
+            lblFecha.BackColor = Color.White;
+            lblFecha.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblFecha.Location = new Point(24, 49);
             lblFecha.Name = "lblFecha";
-            lblFecha.Size = new Size(38, 15);
+            lblFecha.Size = new Size(50, 21);
             lblFecha.TabIndex = 0;
             lblFecha.Text = "Fecha";
             // 
             // lblProducto
             // 
             lblProducto.AutoSize = true;
-            lblProducto.Location = new Point(106, 158);
+            lblProducto.BackColor = Color.White;
+            lblProducto.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblProducto.Location = new Point(24, 95);
             lblProducto.Name = "lblProducto";
-            lblProducto.Size = new Size(56, 15);
+            lblProducto.Size = new Size(73, 21);
             lblProducto.TabIndex = 1;
             lblProducto.Text = "Producto";
             // 
             // lblCantidad
             // 
             lblCantidad.AutoSize = true;
-            lblCantidad.Location = new Point(106, 211);
+            lblCantidad.BackColor = Color.White;
+            lblCantidad.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCantidad.Location = new Point(24, 145);
             lblCantidad.Name = "lblCantidad";
-            lblCantidad.Size = new Size(55, 15);
+            lblCantidad.Size = new Size(72, 21);
             lblCantidad.TabIndex = 2;
             lblCantidad.Text = "Cantidad";
             // 
             // lblPrecioUnitario
             // 
             lblPrecioUnitario.AutoSize = true;
-            lblPrecioUnitario.Location = new Point(106, 272);
+            lblPrecioUnitario.BackColor = Color.White;
+            lblPrecioUnitario.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPrecioUnitario.Location = new Point(24, 199);
             lblPrecioUnitario.Name = "lblPrecioUnitario";
-            lblPrecioUnitario.Size = new Size(85, 15);
+            lblPrecioUnitario.Size = new Size(113, 21);
             lblPrecioUnitario.TabIndex = 3;
             lblPrecioUnitario.Text = "Precio Unitario";
             // 
             // dtpFecha
             // 
-            dtpFecha.Location = new Point(258, 91);
+            dtpFecha.Format = DateTimePickerFormat.Short;
+            dtpFecha.Location = new Point(145, 49);
             dtpFecha.Name = "dtpFecha";
-            dtpFecha.Size = new Size(200, 23);
+            dtpFecha.Size = new Size(83, 23);
             dtpFecha.TabIndex = 4;
             // 
             // cmbProducto
             // 
             cmbProducto.FormattingEnabled = true;
-            cmbProducto.Location = new Point(258, 155);
+            cmbProducto.Location = new Point(145, 97);
             cmbProducto.Name = "cmbProducto";
-            cmbProducto.Size = new Size(121, 23);
+            cmbProducto.Size = new Size(142, 23);
             cmbProducto.TabIndex = 5;
-            // 
-            // cmbCantidad
-            // 
-            cmbCantidad.FormattingEnabled = true;
-            cmbCantidad.Location = new Point(258, 208);
-            cmbCantidad.Name = "cmbCantidad";
-            cmbCantidad.Size = new Size(121, 23);
-            cmbCantidad.TabIndex = 6;
             // 
             // txtPrecioUnitario
             // 
-            txtPrecioUnitario.Location = new Point(259, 270);
+            txtPrecioUnitario.Location = new Point(166, 197);
             txtPrecioUnitario.Name = "txtPrecioUnitario";
             txtPrecioUnitario.Size = new Size(100, 23);
             txtPrecioUnitario.TabIndex = 7;
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.BackColor = Color.White;
+            btnRegistrar.Location = new Point(273, 298);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(128, 51);
+            btnRegistrar.TabIndex = 8;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = false;
+            // 
+            // txtCantidad
+            // 
+            txtCantidad.Location = new Point(155, 148);
+            txtCantidad.Name = "txtCantidad";
+            txtCantidad.Size = new Size(100, 23);
+            txtCantidad.TabIndex = 9;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.BackColor = Color.White;
+            btnCancelar.Location = new Point(113, 298);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(128, 51);
+            btnCancelar.TabIndex = 10;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.AppWorkspace;
+            ClientSize = new Size(511, 450);
+            Controls.Add(btnCancelar);
+            Controls.Add(txtCantidad);
+            Controls.Add(btnRegistrar);
             Controls.Add(txtPrecioUnitario);
-            Controls.Add(cmbCantidad);
             Controls.Add(cmbProducto);
             Controls.Add(dtpFecha);
             Controls.Add(lblPrecioUnitario);
@@ -118,7 +151,7 @@
             Controls.Add(lblProducto);
             Controls.Add(lblFecha);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Registro de ventas";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -131,7 +164,9 @@
         private Label lblPrecioUnitario;
         private DateTimePicker dtpFecha;
         private ComboBox cmbProducto;
-        private ComboBox cmbCantidad;
         private TextBox txtPrecioUnitario;
+        private Button btnRegistrar;
+        private TextBox txtCantidad;
+        private Button btnCancelar;
     }
 }
