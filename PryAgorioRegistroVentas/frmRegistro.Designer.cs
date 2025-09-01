4655a982fb04b8664628dd9a5b7a4ca9f851e0de
Alumno <Alumno@PC15>
2025-09-01 13:21:44 -0300

    Agregar archivos de proyecto.

---
+++ PryAgorioRegistroVentas/frmRegistro.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace PryAgorioRegistroVentas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblFecha = new Label();
+            lblProducto = new Label();
+            lblCantidad = new Label();
+            lblPrecioUnitario = new Label();
+            dtpFecha = new DateTimePicker();
+            cmbProducto = new ComboBox();
+            cmbCantidad = new ComboBox();
+            txtPrecioUnitario = new TextBox();
+            SuspendLayout();
+            // 
+            // lblFecha
+            // 
+            lblFecha.AutoSize = true;
+            lblFecha.Location = new Point(106, 92);
+            lblFecha.Name = "lblFecha";
+            lblFecha.Size = new Size(38, 15);
+            lblFecha.TabIndex = 0;
+            lblFecha.Text = "Fecha";
+            // 
+            // lblProducto
+            // 
+            lblProducto.AutoSize = true;
+            lblProducto.Location = new Point(106, 158);
+            lblProducto.Name = "lblProducto";
+            lblProducto.Size = new Size(56, 15);
+            lblProducto.TabIndex = 1;
+            lblProducto.Text = "Producto";
+            // 
+            // lblCantidad
+            // 
+            lblCantidad.AutoSize = true;
+            lblCantidad.Location = new Point(106, 211);
+            lblCantidad.Name = "lblCantidad";
+            lblCantidad.Size = new Size(55, 15);
+            lblCantidad.TabIndex = 2;
+            lblCantidad.Text = "Cantidad";
+            // 
+            // lblPrecioUnitario
+            // 
+            lblPrecioUnitario.AutoSize = true;
+            lblPrecioUnitario.Location = new Point(106, 272);
+            lblPrecioUnitario.Name = "lblPrecioUnitario";
+            lblPrecioUnitario.Size = new Size(85, 15);
+            lblPrecioUnitario.TabIndex = 3;
+            lblPrecioUnitario.Text = "Precio Unitario";
+            // 
+            // dtpFecha
+            // 
+            dtpFecha.Location = new Point(258, 91);
+            dtpFecha.Name = "dtpFecha";
+            dtpFecha.Size = new Size(200, 23);
+            dtpFecha.TabIndex = 4;
+            // 
+            // cmbProducto
+            // 
+            cmbProducto.FormattingEnabled = true;
+            cmbProducto.Location = new Point(258, 155);
+            cmbProducto.Name = "cmbProducto";
+            cmbProducto.Size = new Size(121, 23);
+            cmbProducto.TabIndex = 5;
+            // 
+            // cmbCantidad
+            // 
+            cmbCantidad.FormattingEnabled = true;
+            cmbCantidad.Location = new Point(258, 208);
+            cmbCantidad.Name = "cmbCantidad";
+            cmbCantidad.Size = new Size(121, 23);
+            cmbCantidad.TabIndex = 6;
+            // 
+            // txtPrecioUnitario
+            // 
+            txtPrecioUnitario.Location = new Point(259, 270);
+            txtPrecioUnitario.Name = "txtPrecioUnitario";
+            txtPrecioUnitario.Size = new Size(100, 23);
+            txtPrecioUnitario.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtPrecioUnitario);
+            Controls.Add(cmbCantidad);
+            Controls.Add(cmbProducto);
+            Controls.Add(dtpFecha);
+            Controls.Add(lblPrecioUnitario);
+            Controls.Add(lblCantidad);
+            Controls.Add(lblProducto);
+            Controls.Add(lblFecha);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblFecha;
+        private Label lblProducto;
+        private Label lblCantidad;
+        private Label lblPrecioUnitario;
+        private DateTimePicker dtpFecha;
+        private ComboBox cmbProducto;
+        private ComboBox cmbCantidad;
+        private TextBox txtPrecioUnitario;
+    }
+}
